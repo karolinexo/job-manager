2c9ead539d85ea5b6c1c1d38ab2588f313178e24
karolinexo <mariakarolinexo@gmail.com>
2026-05-02 20:05:04 -0300

    feat(candidate): expose create endpoint and apply entity validation

--- src/main/java/br/com/mariakaroline/gestaoVagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/mariakaroline/gestaoVagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,27 @@
 package br.com.mariakaroline.gestaoVagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank()
@@ -25,5 +35,8 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/br/com/mariakaroline/gestaoVagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/mariakaroline/gestaoVagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,12 @@
 package br.com.mariakaroline.gestaoVagas.modules.candidate.controllers;
 
+import br.com.mariakaroline.gestaoVagas.exceptions.UserFoundException;
 import br.com.mariakaroline.gestaoVagas.modules.candidate.CandidateEntity;
+import br.com.mariakaroline.gestaoVagas.modules.candidate.CandidateRepository;
+import br.com.mariakaroline.gestaoVagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
